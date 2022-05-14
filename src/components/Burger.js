45e5c166a42afb982cf2a776825438e01049598a
Faisal Kabir <onlyfaisalkabir@gmail.com>
2022-05-14 12:49:56 +0600

    initial commit

---
+++ src/components/Burger.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import './BurgerStyle.css';
+
+
+export default class Burger extends Component {
+    state = {
+        Lettuce: 0,
+        Tomato: 0,
+        Cheese: 0,
+        Meat: 0
+    }
+    render() {
+        return (
+            <>
+                <div className="burgerIngredients">
+                    Burger
+
+                </div>
+                <div className="ingredientsBlock">
+                    ingredients
+                </div>
+            </>
+        )
+    }
+}
